5e07e4cf10c1885b0ec07a4286d926a8c8e06238
Aaron Na <syn228@nyu.edu>
2018-04-15 22:09:52 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday.collect do |holiday, supply|
       if supply.include?("BBQ")
       return holiday.flatten
-      
+    end
     end
   end
 end
