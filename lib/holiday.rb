3e01e270d5eaf59397f3cb02796a93ed1dadb001
Aaron Na <syn228@nyu.edu>
2018-04-15 21:40:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,7 @@ def all_winter_holiday_supplies(holiday_hash)
   
   holiday_hash[:winter].each do |holiday, supply|
     return supply
-  
+  end
 end
 
 def all_supplies_in_holidays(holiday_hash)
