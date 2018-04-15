c3a87e9dcf1c6f110c0aca747d233fb5b1a6eb18
Aaron Na <syn228@nyu.edu>
2018-04-15 22:09:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.collect do |season, holiday|
     holiday.collect do |holiday, supply|
-      holiday if supply.include?("BBQ")
+      holiday.flatten if supply.include?("BBQ")
       
       
     end
